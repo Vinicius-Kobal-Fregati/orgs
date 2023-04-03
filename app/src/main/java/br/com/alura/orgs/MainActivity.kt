b16347d90329e8be40c32e2db67527d65dd58dce
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-03 12:34:43 -0300

    Adicionando constraint

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,9 +2,6 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
