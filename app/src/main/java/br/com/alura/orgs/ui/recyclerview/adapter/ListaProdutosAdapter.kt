fd3cf0895bd78b08a88cd04a46f4ecc7e450b10c
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-03 23:17:42 -0300

    Implementando os metodos do adapter

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,20 +1,49 @@
 package br.com.alura.orgs.ui.recyclerview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Produto
 
 class ListaProdutosAdapter(
+    private val context: Context,
     private val produtos: List<Produto>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun vincula(produto: Produto) {
+            // Esse itemView é a view que mandamos para ele no construtor.
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            descricao.text = produto.descricao
+            val valor = itemView.findViewById<TextView>(R.id.valor)
+            valor.text = produto.valor.toPlainString()
+        }
+
+    }
+
     // Cria a referência viewHolder (resposável por fazer o bind das views)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        // Forma de transformar um layout em uma view, inflando ele
+        val inflater = LayoutInflater.from(context)
+        // O attachToRoot precisa ser falso para o RecyclerView poder ter o controle do que exibir
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(view)
     }
 
     // Indica qual o item que o adapter está colocando, indicando sua posição
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 
     // Determina quantos itens queremos exibir dentro dele
