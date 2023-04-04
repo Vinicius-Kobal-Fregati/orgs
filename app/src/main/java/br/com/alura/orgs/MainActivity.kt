0bbb12a5f4d8e0b5c2d775bd22e76ca87ca7023f
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-03 22:14:59 -0300

    Buscando views do layout na activity

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -10,5 +11,14 @@ class MainActivity : Activity() {
 
         //O R pode acessar tudo que está dentro de resources
         setContentView(R.layout.activity_main)
+
+        // Processo de bind, vinculação entre o código fonte e o arquivo de layout
+        val nome = findViewById<TextView>(R.id.nome)
+        val descricao = findViewById<TextView>(R.id.descricao)
+        val valor = findViewById<TextView>(R.id.valor)
+
+        nome.text = "Cesta"
+        descricao.text = "Laranja, manga e maçã"
+        valor.text = "19.99"
     }
 }
