77b1896af3adb9dd56b98941eaa011491008ed51
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-04 14:54:34 -0300

    Configurando listener de clique

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,16 +1,27 @@
 package br.com.alura.orgs.ui.activity
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.R
+import kotlin.math.log
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
-    /*
     // Com o AppCompatActivity, podemos deixar de usar o setContentView passando
     // para seu construtor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
+        //setContentView(R.layout.activity_formulario_produto)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", nome)
+        }
     }
-     */
 }
