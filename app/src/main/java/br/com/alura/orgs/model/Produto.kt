c103020bdef90b6f26188def170429d22dec768b
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-08 10:51:33 -0300

    Implementando entidade e dao

--- app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -2,12 +2,15 @@ package br.com.alura.orgs.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 @Entity
 @Parcelize
 data class Produto(
+    // Usa-se o padrão zero para ele auto-incrementar
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
     val nome: String,
     val descricao: String,
     val valor: BigDecimal,
