e054b179894c0a577ef2129534e7bef0aca1b862
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-09 11:20:22 -0300

    Criando o menu de opcoes e o item de edicao

--- app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -1,7 +1,10 @@
 package br.com.alura.orgs.ui.activity
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
 import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.alura.orgs.extension.formataParaMoedaBrasileira
 import br.com.alura.orgs.extension.tentaCarregarImagem
@@ -19,15 +22,20 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         tentaCarregarProduto()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     private fun tentaCarregarProduto() {
         intent.getParcelableExtra<Produto>(CHAVE_PRODUTO)?.let { produtoCarregado ->
-            preencheCampos(produtoCarregado)
+            preencheCampos(produtoCarregado, this)
         } ?: finish()
     }
 
-    private fun preencheCampos(produtoCarregado: Produto) {
+    private fun preencheCampos(produtoCarregado: Produto, contexto: Context) {
         with(binding) {
-            activityDetalhesProdutoImageview.tentaCarregarImagem(produtoCarregado.imagem)
+            activityDetalhesProdutoImageview.tentaCarregarImagem(produtoCarregado.imagem, contexto)
             activityDetalhesProdutoNome.text = produtoCarregado.nome
             activityDetalhesProdutoDescricao.text = produtoCarregado.descricao
             activityDetalhesProdutoValor.text =
