162f6d806761392b3b23259eed604f893381567d
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-04-10 16:10:27 -0300

    Reaproveitando instancia do database

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -17,15 +17,26 @@ abstract class AppDatabase : RoomDatabase() {
 
     // Como nossa classe é abstrata, precisamos que o método seja estático para podermos acessar ele
     // Para isso, usamos o companion object
+    // A instanciação do room é custosa, por isso, é indicado reutilizar ela (Singleton)
     companion object {
+
+        // Indica para todas as threads que o valor é inserido na property
+        // Assim, outras threads não vão atribuir um novo valor para ele
+        // Garante a estabilidade, integridade e unicidade do singleton
+        @Volatile
+        private lateinit var db: AppDatabase
+
         fun instancia(contexto: Context): AppDatabase {
+            if (::db.isInitialized) return db
             return Room.databaseBuilder(
                 contexto,
                 AppDatabase::class.java,
                 "orgs.db"
             )
                 .allowMainThreadQueries()
-                .build()
+                .build().also {
+                    db = it
+                }
         }
     }
 }
